cddee63312c296058630d5bda4c0afd74b031614
Unknown <slyguggi@gmail.com>
2018-12-20 16:12:12 +0100

    Added basic ship. moves at constant speed

---
+++ Assets/Scripts/ShipController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class ShipController : MonoBehaviour
+{
+	private Rigidbody2D myRigidbody2D;
+	[SerializeField] private float speed = 2.0f;
+
+	private void Start()
+	{
+		myRigidbody2D = GetComponent<Rigidbody2D>();
+	}
+
+	private void Update()
+	{
+		myRigidbody2D.velocity = myRigidbody2D.velocity.normalized * speed;
+	}
+}
