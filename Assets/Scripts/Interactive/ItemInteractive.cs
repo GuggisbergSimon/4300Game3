978211a703294642b359448693e1f00571c315ac
Simon <slyguggi@gmail.com>
2018-12-23 19:31:18 +0100

    dialogues can follow each other now

--- Assets/Scripts/Interactive/ItemInteractive.cs
+++ Assets/Scripts/Interactive/ItemInteractive.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class ItemInteractive : Interactive
 {
-	[SerializeField] private Text text;
+	[SerializeField] private Text[] text;
 	private bool isInteracting = false;
+	private int currentTextIndex;
 	
 	public override void Interact()
 	{
 		base.Interact();
-		GameManager.Instance.MyUiManager.DisplayDialogue(text);
+		currentTextIndex = 0;
+		GameManager.Instance.MyUiManager.DisplayDialogue(text[currentTextIndex]);
 		StartCoroutine(IsInteractingNextFrame());
 	}
 
@@ -25,15 +27,24 @@ public class ItemInteractive : Interactive
 		//checks for closing dialogue panel
 		if (isInteracting && Input.GetButtonDown("Fire1") && !GameManager.Instance.MyUiManager.IsPrinting)
 		{
-			GameManager.Instance.MyUiManager.CloseDialogue();
-			GameManager.Instance.Player.CanPlayerMove = true;
 			isInteracting = false;
+			if (currentTextIndex == text.Length - 1)
+			{
+				GameManager.Instance.MyUiManager.CloseDialogue();
+				GameManager.Instance.Player.CanPlayerMove = true;
+			}
+			else
+			{
+				currentTextIndex++;
+				GameManager.Instance.MyUiManager.DisplayDialogue(text[currentTextIndex]);
+				StartCoroutine(IsInteractingNextFrame());
+			}
 		}
 
 		//checks for skipping dialogue
-		if (isInteracting && Input.GetButtonDown("Fire1") && text.skippable)
+		if (isInteracting && Input.GetButtonDown("Fire1") && text[currentTextIndex].skippable && GameManager.Instance.MyUiManager.IsPrinting)
 		{
-			GameManager.Instance.MyUiManager.DisplayAllText(text.text);
+			GameManager.Instance.MyUiManager.DisplayAllText(text[currentTextIndex].text);
 		}
 	}
 }
