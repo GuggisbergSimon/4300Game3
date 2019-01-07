1e4d19490fb24f949b90abf590bd95031c468931
Simon <slyguggi@gmail.com>
2019-01-07 16:38:56 +0100

    Added music in scene and mixers, adjusted loop

---
+++ Assets/Scripts/Manager/MusicManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+	[SerializeField] private AudioClip intro;
+	[SerializeField] private AudioClip loop;
+	[SerializeField] private AudioClip outro;
+	private AudioSource myAudioSource;
+
+	private void Start()
+	{
+		myAudioSource = GetComponent<AudioSource>();
+		ChangeAudioClipNoLoop(intro);
+		StartCoroutine(ChangeAudioClipWithLoop(loop));
+	}
+
+	private void ChangeAudioClipNoLoop(AudioClip clip)
+	{
+		myAudioSource.clip = clip;
+		myAudioSource.Play();
+		myAudioSource.loop = false;
+	}
+
+	public void PlayOutro()
+	{
+		ChangeAudioClipNoLoop(outro);
+	}
+	
+	private IEnumerator ChangeAudioClipWithLoop(AudioClip clip)
+	{
+		yield return new WaitForSeconds(myAudioSource.clip.length);
+		ChangeAudioClipNoLoop(clip);
+		myAudioSource.loop = true;
+	}
+}
